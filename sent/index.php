3f3b401c0008805896cbe6c56c8e2ab560a50c12
BartJongejan <bart.jongejan@gmail.com>
2020-03-05 13:52:58 +0100

    $dodelete -> true

--- sent/index.php
+++ sent/index.php
@@ -35,7 +35,7 @@ Inactive       :
 $toollog = '/opt/texton/log/sent.log'; /* Used by the logit() function. TODO make sure the folder exists and is writable. Adapt if needed */
                 
 /*  TODO Set $dodelete to false if temporary files in /tmp should not be deleted before returning. */
-$dodelete = false;
+$dodelete = true;
 $tobedeleted = array();
 
 
