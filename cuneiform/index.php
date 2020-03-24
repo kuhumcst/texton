5eaa47b6e4358e468eaed51ce9086e5bfc88385c
BartJongejan <bart.jongejan@gmail.com>
2020-03-24 08:36:08 +0100

    Added cuneiform wrapper

---
+++ cuneiform/index.php
@@ -0,0 +1,8 @@
+<?php
+header("Content-type:text/plain; charset=UTF-8");
+// Next line for stopping UTF-8 related complaints from perl (called with system())!
+// (See file /etc/sysconfig/i18n)
+ putenv("LANG=da_DK");
+include_once ("cuneiform.inc");
+?>
+
