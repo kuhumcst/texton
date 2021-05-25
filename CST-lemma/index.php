a7394bc0a7606aff9bae654f81c0af0f49bbdeab
zgk261 <bartj@hum.ku.dk>
2021-05-25 18:23:42 +0000

    Removed CR

--- CST-lemma/index.php
+++ CST-lemma/index.php
@@ -353,7 +353,7 @@ function lemmatiser($Oformatflat,$Ofacetlem,$Ofacetsl,$Ofacetstl,$Ofacetstlp,$Of
         }
     else if($language == 'pl')
         {
-        $flexrulessubdir = "0";
+        $flexrulessubdir = "/0";
         $flexrules = "flexrules.polimorfologik.txt.ph_ziggurat_XC";
         }
     else if($language == 'pt')
@@ -417,8 +417,6 @@ function lemmatiser($Oformatflat,$Ofacetlem,$Ofacetsl,$Ofacetstl,$Ofacetstlp,$Of
         $H = "0";
         logit("Uminus $Uminus");
         } 
-
-    logit("c:".$c);
 
     if($Ipresnml && !$Opresnml || $ShowTag) 
         {
