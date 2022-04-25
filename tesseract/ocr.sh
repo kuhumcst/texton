0845485500e71f9fbde5383c26c1e6883bdbaae4
Bart Jongejan <bart.jongejan@gmail.com>
2022-04-25 12:44:14 +0200

    Replaced Imagemagick 'convert' with xpdf 'pdftoppm'

--- tesseract/ocr.sh
+++ tesseract/ocr.sh
@@ -36,24 +36,28 @@ pushd $TMP_DIR
 #convert -verbose -density 200 -trim * -quality 100 -sharpen 0x1.0 -alpha Off -depth 8 -colorspace gray p%02d.tif
 #convert -verbose -trim * -quality 100 -sharpen 0x1.0 -alpha Off -depth 8 -colorspace gray p%02d.tif
 #convert -verbose -trim -quality 100 -sharpen 0x1.0 -alpha Off -density 300 * p%02d.tif
-convert -verbose -trim -quality 100 -alpha Off -density 300 * p%02d.tif #***** -density 300 is sometimes too high, something chokes.
+#SECURITY RISKS WITH IMAGEMAGICK+GHOSTSCRIPT FORMATS GS AND PDF! convert -verbose -trim -quality 100 -alpha Off -density 300 * p%02d.tif #***** -density 300 is sometimes too high, something chokes.
 #convert -verbose -trim -quality 100 -alpha Off -density 200 * p%02d.tif
 
+pdftoppm $1 p
  
 # For every TIFF file listed in numerical order in the temporary
 # directory (contd)
  
-for i in `ls *.tif | sort -tp -k2n`;
+##for i in `ls *.tif | sort -tp -k2n`;
+for i in `ls *.ppm | sort -tp -k3n`;
  
 do
  
 # strip away full path to file and file extension
  
- BASE=`basename "$i" .tif`;
+## BASE=`basename "$i" .tif`;
+ BASE=`basename "$i" .ppm`;
  
 # run Tesseract using the English language on each TIFF file
  
- tesseract --tessdata-dir $3 "${BASE}.tif" "${BASE}" -l $2
+## tesseract --tessdata-dir $3 "${BASE}.tif" "${BASE}" -l $2
+ tesseract --tessdata-dir $3 "${BASE}.ppm" "${BASE}" -l $2
  
 # append output of each resulting TXT file into an output file with
 # pagebreak marks at then end of each page
@@ -63,13 +67,13 @@ do
  
 # remove all TIFF and TXT files
  
- rm ${BASE}.*;
+rm ${BASE}.*;
  
 done
  
 # remove any remaining files (eg. PDF, etc.)
  
- rm *
+rm *
  
 # change to parent directory
  
